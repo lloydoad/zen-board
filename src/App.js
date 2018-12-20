c00107a03323c9cf34b240d0f0a54bd469220ef1
Lloyd Dapaah <lloyd.dapaah@yahoo.com>
2018-12-20 15:25:30 -0500

    reduce fetch requests, format date and time, other minor request fixes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import NewsDisplayPanel from './components/NewsPanel/NewsDisplayPanel';
 
 import './App.css';
 
+const API_KEY = "93864cfe75afb310af5519fc66a90d91";
+
 class App extends Component {
   constructor() {
     super();
@@ -23,36 +25,78 @@ class App extends Component {
         width: "65%"
       },
       weatherPanelInfo: {
-        temperature: "13°",
-        location: "Orlando",
+        temperature: "-",
+        location: "-",
         width: "33%"
-      }
+      },
+      countryCode: undefined
     }
   }
 
   componentDidMount() {
+    this.tick();
     this.timeID = setInterval(
       ()=>this.tick(), 
-      100
+      60000
+    );
+    this.updateWeather();
+    this.weatherID = setInterval(
+      ()=>this.updateWeather(),
+      3600000
     );
   }
 
   componentWillUnmount() {
     clearInterval(this.timeID);
+    clearInterval(this.weatherID);
   }
 
   tick() {
     const { timePanelInfo } = this.state;
 
-    var options = { year: 'numeric', month: 'long', day: 'numeric' };
-    var timeOptions = { hour12: true }
-    var today  = new Date();
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const timeOptions = { hour12: true }
+    let today  = new Date();
     timePanelInfo.day = today.toLocaleDateString("en-US", options);
-    timePanelInfo.time = today.toLocaleTimeString("en-US", timeOptions);
 
-    console.log("running");
+    const timeFormatted = today.toLocaleTimeString("en-US", timeOptions).split(":");
+    const meridian = timeFormatted[2].split(" ")[1];
+
+    timePanelInfo.time = `${timeFormatted[0]}:${timeFormatted[1]} ${meridian}`;
 
     this.setState({timePanelInfo:timePanelInfo});
+  }
+
+  updateWeather() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position)=>{
+        const lat = position.coords.latitude;
+        const long = position.coords.longitude;
+        const proxyForCors = `https://cors-anywhere.herokuapp.com/`;
+        const fetchAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=imperial`;
+
+        const startWeatherUpdate = async () => {
+          const api_call = await fetch(`${proxyForCors}${fetchAPI}`);
+          if(api_call.ok) {
+            const { weatherPanelInfo } = this.state;
+            const data = await api_call.json();
+
+            const countryCode = `${data.sys.country}`;
+            const strRep = `${data.main.temp}`
+            const dateTokens = strRep.split(".");
+            weatherPanelInfo.temperature = `${dateTokens[0]}°`;
+            weatherPanelInfo.location = data.name;
+            
+            this.setState({
+                weatherPanelInfo : weatherPanelInfo,
+                countryCode: countryCode.toLowerCase()
+            });
+          }
+        }; 
+
+        startWeatherUpdate();
+      });
+    }
   }
 
   respondToMenuBtn = (panelName) => {
